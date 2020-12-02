a77bca94641e326ce684f07a816dbe3818f92022
SkYContact <416019315@qq.com>
2020-12-02 19:32:53 +0800

    remake UE

--- MyDay/MyDay.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyDay.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyDay, "MyDay" );
