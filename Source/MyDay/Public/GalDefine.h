b3832a51b90adccb26c62d89d9811fb929205cc1
SkYContact <416019315@qq.com>
2020-12-02 19:26:08 +0800

    remake UE

---
+++ Source/MyDay/Public/GalDefine.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EGalEmotion : uint8
+{
+	ENormal,
+	EHappy,
+	ESad,
+	EAngry,
+	ESuprise,
+	EFear,
+	ELisk,
+	EDisLike,
+};
